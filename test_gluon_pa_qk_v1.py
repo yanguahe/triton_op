0b67a54c3b108aa024d7b868b4a1b1a9b5864627
yanguahe <yanguahe@amd.com>
2025-09-15 10:28:14 +0800

    add gluon_pa_qk 2 version impl

--- test_gluon_pa_qk_v1.py
+++ test_gluon_pa_qk_v1.py
@@ -135,7 +135,7 @@ def compare_arrays(arr1: np.ndarray, arr2: np.ndarray,
 def get_autotune_config():
     sizes = [
         # {'QUERY_GRP_SZ': 16, 'SEQ_PARTITION_SZ': 256, 'SEQ_PARTITION_KV_BLK_NUM': 16, 'K_HD_SPLIT_NUM': 16, 'K_SPLIT_HEAD_SZ': 8, 'KV_BLK_SZ': 16, 'HEAD_SZ': 128},
-        {'QUERY_GRP_SZ': 16, 'SEQ_PARTITION_SZ': 256, 'SEQ_PARTITION_KV_BLK_NUM': 16, 'K_HD_SPLIT_NUM': 16, 'K_SPLIT_HEAD_SZ': 1, 'KV_BLK_SZ': 16, 'HEAD_SZ': 128},
+        {'QUERY_GRP_SZ': 8, 'SEQ_PARTITION_SZ': 256, 'SEQ_PARTITION_KV_BLK_NUM': 16, 'K_HD_SPLIT_NUM': 16, 'K_SPLIT_HEAD_SZ': 8, 'KV_BLK_SZ': 16, 'HEAD_SZ': 128},
     ]
     return [triton.Config(s) for s in sizes]
 
@@ -149,18 +149,17 @@ def gemm_qk(
     k_ptr,
     qk_ptr,
     kv_len,
+    k_split_head_sz,
     q_stride0,
     q_stride1,
     q_stride2,
     q_stride3,
-    q_stride4,
     k_stride0,
     k_stride1,
     k_stride2,
     k_stride3,
     k_stride4,
     k_stride5,
-    k_stride6,
     qk_stride0,
     qk_stride1,
     qk_stride2,
@@ -203,7 +202,7 @@ def gemm_qk(
     # max_num_kv_blks: gl.constexpr = (SEQ_PARTITION_SZ + KV_BLK_SZ - 1) // KV_BLK_SZ
     # num_kv_blks = gl.cdiv(seq_end_idx - seq_start_idx, KV_BLK_SZ)
 
-    # 1 x QUERY_GRP_SZ x K_HD_SPLIT_NUM
+    # 1 x QUERY_GRP_SZ x (K_HD_SPLIT_NUM x K_SPLIT_HEAD_SZ)
     # 1 x 8(mdim) x 16(kdim)
     blocked_q: gl.constexpr = gl.BlockedLayout(
         size_per_thread =[1, 1, 1],
@@ -211,7 +210,7 @@ def gemm_qk(
         warps_per_cta   =[1, 2, 2],
         order           =[2, 1, 0],
     )
-    # SEQ_PARTITION_KV_BLK_NUM x K_HD_SPLIT_NUM x KV_BLK_SZ
+    # SEQ_PARTITION_KV_BLK_NUM x (K_HD_SPLIT_NUM x K_SPLIT_HEAD_SZ) x KV_BLK_SZ
     # 16 x 16(kdim) x 16(ndim)
     blocked_k: gl.constexpr = gl.BlockedLayout(
         size_per_thread =[4, 4, 1],
@@ -244,50 +243,25 @@ def gemm_qk(
     # offs_q_dim0 = gl.arange(0, SEQ_PARTITION_KV_BLK_NUM, layout=q_dim_0_layout)
     offs_q_dim0 = gl.arange(0, 1, layout=q_dim_0_layout)
     offs_q_dim1 = gl.arange(0, QUERY_GRP_SZ, layout=q_dim_1_layout)
-    offs_q_dim2 = gl.arange(0, K_HD_SPLIT_NUM, layout=q_dim_2_layout)
+    offs_q_dim2 = gl.arange(0, HEAD_SZ, layout=q_dim_2_layout)
     offs_k_dim0 = gl.arange(0, SEQ_PARTITION_KV_BLK_NUM, layout=k_dim_0_layout)
-    offs_k_dim1 = gl.arange(0, K_HD_SPLIT_NUM, layout=k_dim_1_layout)
+    offs_k_dim1 = gl.arange(0, HEAD_SZ, layout=k_dim_1_layout)
     offs_k_dim2 = gl.arange(0, KV_BLK_SZ, layout=k_dim_2_layout)
     offs_q = offs_q_dim0[:, None, None] * 0 + offs_q_dim1[None, :, None] * q_stride2 + offs_q_dim2[None, None, :] * q_stride3
     offs_k = offs_k_dim0[:, None, None] * k_stride2 + offs_k_dim1[None, :, None] * k_stride4 + offs_k_dim2[None, None, :] * k_stride5
 
-    # accumulator = gl.zeros((SEQ_PARTITION_KV_BLK_NUM, QUERY_GRP_SZ, KV_BLK_SZ), dtype=gl.float32, layout=mfma_layout)
-    # # for k in range(0, K_SPLIT_HEAD_SZ):
-    # for k in range(0, 2):
-    #     # cur_q_ptr = q_ptr + k * q_stride4
-    #     # cur_k_ptr = k_ptr + k * k_stride6
-    #     # a = gl.amd.cdna3.buffer_load(ptr=q_ptr, offsets=offs_q, mask=offs_q_dim2[None, None, :] < K_HD_SPLIT_NUM)
-    #     # b = gl.amd.cdna3.buffer_load(ptr=k_ptr, offsets=offs_k, mask=offs_k_dim2[None, None, :] < KV_BLK_SZ)
-    #     a = gl.amd.cdna3.buffer_load(ptr=q_ptr, offsets=offs_q)
-    #     b = gl.amd.cdna3.buffer_load(ptr=k_ptr, offsets=offs_k)
-    #     a_broadcasted = tl.broadcast_to(a, SEQ_PARTITION_KV_BLK_NUM, QUERY_GRP_SZ, K_HD_SPLIT_NUM)
-    #     a1 = gl.convert_layout(a_broadcasted, layout=dot_a_layout)
-    #     b1 = gl.convert_layout(b, layout=dot_b_layout)
-    #     accumulator = gl.amd.cdna3.mfma(a1, b1, accumulator)
-    #     q_ptr += q_stride4
-    #     k_ptr += k_stride6
-
-
-    acc0 = gl.zeros((SEQ_PARTITION_KV_BLK_NUM, QUERY_GRP_SZ, KV_BLK_SZ), dtype=gl.float32, layout=mfma_layout)
-    acc1 = gl.zeros((SEQ_PARTITION_KV_BLK_NUM, QUERY_GRP_SZ, KV_BLK_SZ), dtype=gl.float32, layout=mfma_layout)
-    a = gl.amd.cdna3.buffer_load(ptr=q_ptr, offsets=offs_q)
-    b = gl.amd.cdna3.buffer_load(ptr=k_ptr, offsets=offs_k)
-    a_broadcasted = tl.broadcast_to(a, SEQ_PARTITION_KV_BLK_NUM, QUERY_GRP_SZ, K_HD_SPLIT_NUM)
-    a1 = gl.convert_layout(a_broadcasted, layout=dot_a_layout)
-    b1 = gl.convert_layout(b, layout=dot_b_layout)
-    out0 = gl.amd.cdna3.mfma(a1, b1, acc0)
-    q_ptr += q_stride4
-    k_ptr += k_stride6
-
-    a = gl.amd.cdna3.buffer_load(ptr=q_ptr, offsets=offs_q)
-    b = gl.amd.cdna3.buffer_load(ptr=k_ptr, offsets=offs_k)
-    a_broadcasted = tl.broadcast_to(a, SEQ_PARTITION_KV_BLK_NUM, QUERY_GRP_SZ, K_HD_SPLIT_NUM)
-    a1 = gl.convert_layout(a_broadcasted, layout=dot_a_layout)
-    b1 = gl.convert_layout(b, layout=dot_b_layout)
-    out1 = gl.amd.cdna3.mfma(a1, b1, acc1)
-    q_ptr += q_stride4
-    k_ptr += k_stride6
-    accumulator = out0 + out1
+    accumulator = gl.zeros((SEQ_PARTITION_KV_BLK_NUM, QUERY_GRP_SZ, KV_BLK_SZ), dtype=gl.float32, layout=mfma_layout)
+    for k in range(0, gl.cdiv(k_split_head_sz, HEAD_SZ)):
+        # a = gl.amd.cdna3.buffer_load(ptr=q_ptr, offsets=offs_q, mask=offs_q_dim2[None, None, :] < K_HD_SPLIT_NUM)
+        # b = gl.amd.cdna3.buffer_load(ptr=k_ptr, offsets=offs_k, mask=offs_k_dim1[None, :, None] < K_HD_SPLIT_NUM)
+        a = gl.amd.cdna3.buffer_load(ptr=q_ptr, offsets=offs_q)
+        b = gl.amd.cdna3.buffer_load(ptr=k_ptr, offsets=offs_k)
+        a_broadcasted = tl.broadcast_to(a, SEQ_PARTITION_KV_BLK_NUM, QUERY_GRP_SZ, HEAD_SZ)
+        a1 = gl.convert_layout(a_broadcasted, layout=dot_a_layout)
+        b1 = gl.convert_layout(b, layout=dot_b_layout)
+        accumulator = gl.amd.cdna3.mfma(a1, b1, accumulator)
+        q_ptr += HEAD_SZ * q_stride3
+        k_ptr += HEAD_SZ * k_stride4
 
     qk = accumulator.to(q_ptr.dtype.element_ty)
     offs_qk_dim0 = gl.arange(0, SEQ_PARTITION_KV_BLK_NUM, layout=gl.SliceLayout(1, gl.SliceLayout(2, mfma_layout)))
@@ -306,8 +280,10 @@ def run_gemm_qk(q, k, dtype):
     kv_len = seq_partition_kv_num * SEQ_PARTITION_KV_BLK_NUM * KV_BLK_SZ
     # qk = torch.randn((batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, QUERY_GRP_SZ, KV_BLK_SZ), device="cuda", dtype=dtype)
     qk = torch.empty((batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, QUERY_GRP_SZ, KV_BLK_SZ), device="cuda", dtype=dtype)
-    # print(f"q.stride(4)={q.stride(4)}")
-    # print(f"k.stride(6)={k.stride(6)}")
+    q = q.reshape(batch_size, num_kv_heads, QUERY_GRP_SZ, K_HD_SPLIT_NUM * K_SPLIT_HEAD_SZ)
+    k = k.transpose(-2, -1).reshape(batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, K_HD_SPLIT_NUM * K_SPLIT_HEAD_SZ, KV_BLK_SZ)
+    # print(f"q.stride()={q.stride()}")
+    # print(f"k.stride()={k.stride()}")
 
     grid = (batch_size, num_kv_heads, seq_partition_kv_num)
     gemm_qk[grid](
@@ -315,18 +291,17 @@ def run_gemm_qk(q, k, dtype):
         k,
         qk,
         kv_len,
+        K_SPLIT_HEAD_SZ,
         q.stride(0),
         q.stride(1),
         q.stride(2),
         q.stride(3),
-        q.stride(4),
         k.stride(0),
         k.stride(1),
         k.stride(2),
         k.stride(3),
         k.stride(4),
         k.stride(5),
-        k.stride(6),
         qk.stride(0),
         qk.stride(1),
         qk.stride(2),
@@ -343,7 +318,7 @@ def qk_gemm_ref(q, k):
 
     # Transpose and reshape and K to align with Q's dimensions for matrix multiplication
     k_transposed = k.transpose(-2, -1)  # Swap last two dimensions
-    k_transposed = k_transposed.contiguous()
+    # k_transposed = k_transposed.contiguous()
     k_reshaped = k_transposed.reshape(batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, K_HD_SPLIT_NUM * K_SPLIT_HEAD_SZ, KV_BLK_SZ)
     # Reshape Q for matrix multiplication
     q_reshaped = q.reshape(batch_size, num_kv_heads, QUERY_GRP_SZ, K_HD_SPLIT_NUM * K_SPLIT_HEAD_SZ)
@@ -351,7 +326,7 @@ def qk_gemm_ref(q, k):
     # Perform matrix multiplication using einsum
     # Reduction happens over the last dimension of Q and first of k_transposed (K_HD_SPLIT_NUM * K_SPLIT_HEAD_SZ)
     qk = torch.einsum('bhgd,bsnhdt->bsnhgt', q_reshaped, k_reshaped)
-    qk = qk.contiguous()
+    # qk = qk.contiguous()
     # # Reshape to final output shape
     # qk = qk.reshape(batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, QUERY_GRP_SZ, KV_BLK_SZ)
     return qk
@@ -367,85 +342,78 @@ def test_gemm_qk():
     # )
     # - kv_len = seq_partition_kv_num * SEQ_PARTITION_KV_BLK_NUM * KV_BLK_SZ
     # - K_SPLIT_HEAD_SZ = 8
-    # q_shape_list = [
-    #     [80, 1, 16, 16, 8],
-    # ]
-    # k_shape_list = [
-    #     [80, 16, 16, 1, 16, 16, 8],
-    # ]
     q_shape_list = [
-        [1, 1, 16, 16, 8],
+        [32, 1, 8, 16, 8],
+        # [80, 1, 16, 16, 8],
+        # [80, 3, 16, 16, 8],
     ]
     k_shape_list = [
-        [1, 1, 16, 1, 16, 16, 8],
+        [32, 16, 16, 1, 16, 16, 8],
+        # [80, 16, 16, 1, 16, 16, 8],
+        # [80, 16, 16, 3, 16, 16, 8],
     ]
+    # q_shape_list = [
+    #     [1, 1, 16, 16, 8],
+    # ]
+    # k_shape_list = [
+    #     [1, 1, 16, 1, 16, 16, 8],
+    # ]
     batch_size, num_kv_heads, QUERY_GRP_SZ, K_HD_SPLIT_NUM, K_SPLIT_HEAD_SZ = q_shape_list[0]
     batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, K_HD_SPLIT_NUM, KV_BLK_SZ, K_SPLIT_HEAD_SZ = k_shape_list[0]
     q_len = 1
     kv_len = seq_partition_kv_num * SEQ_PARTITION_KV_BLK_NUM * KV_BLK_SZ
     head_dim_qk = K_HD_SPLIT_NUM * K_SPLIT_HEAD_SZ
-    dtype = torch.float16
+    # dtype = torch.float16
+    dtype = torch.bfloat16
 
     q = torch.randn((batch_size, num_kv_heads, QUERY_GRP_SZ, K_HD_SPLIT_NUM, K_SPLIT_HEAD_SZ), device="cuda", dtype=dtype)
     k = torch.randn((batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, K_HD_SPLIT_NUM, KV_BLK_SZ, K_SPLIT_HEAD_SZ), device="cuda", dtype=dtype)
 
     qk_gn, avg_t_us_gn = run_gemm_qk(q, k, dtype)
 
-    q = q[:, :, :, :, 0:2]
-    k = k[:, :, :, :, :, :, 0:2]
-    q = q.to(torch.float32).contiguous()
-    k = k.to(torch.float32).contiguous()
-    head_dim_qk = K_HD_SPLIT_NUM * 2
-    # qk_ref = qk_gemm_ref(q, k)
-    q = q.reshape(1, QUERY_GRP_SZ, head_dim_qk).contiguous()
-    q_broadcasted = q.repeat(SEQ_PARTITION_KV_BLK_NUM, 1, 1)
-    print(f"k.shape={k.shape}, k.dtype={k.dtype}")
-    k_transposed = k.transpose(-2, -1).contiguous()
-    print(f"k_transposed.shape={k_transposed.shape}, k_transposed.dtype={k_transposed.dtype}")
-    k_transposed = k_transposed.reshape(SEQ_PARTITION_KV_BLK_NUM, head_dim_qk, KV_BLK_SZ)
-    print(f"k_transposed.shape={k_transposed.shape}, k_transposed.dtype={k_transposed.dtype}")
-    print(f"k_transposed.stride()={k_transposed.stride()}")
-    k_transposed = k_transposed.contiguous()
-    print(f"k_transposed.stride()={k_transposed.stride()}")
-    print(f"k_transposed.shape={k_transposed.shape}, k_transposed.dtype={k_transposed.dtype}")
-    k_transposed = k_transposed.reshape(SEQ_PARTITION_KV_BLK_NUM, head_dim_qk, KV_BLK_SZ)
-    qk_ref = torch.bmm(q_broadcasted, k_transposed)
-    qk_ref = qk_ref.reshape(batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, QUERY_GRP_SZ, KV_BLK_SZ)
-    qk_ref = qk_ref.to(dtype)
+    q = q.to(torch.float32)
+    k = k.to(torch.float32)
+    qk_ref = qk_gemm_ref(q, k)
+    # q = q.reshape(1, QUERY_GRP_SZ, head_dim_qk).contiguous()
+    # q_broadcasted = q.repeat(SEQ_PARTITION_KV_BLK_NUM, 1, 1)
+    # k_transposed = k.transpose(-2, -1).contiguous()
+    # k_transposed = k_transposed.reshape(SEQ_PARTITION_KV_BLK_NUM, head_dim_qk, KV_BLK_SZ)
+    # k_transposed = k_transposed.contiguous()
+    # k_transposed = k_transposed.reshape(SEQ_PARTITION_KV_BLK_NUM, head_dim_qk, KV_BLK_SZ)
+    # qk_ref = torch.bmm(q_broadcasted, k_transposed)
+    # qk_ref = qk_ref.reshape(batch_size, seq_partition_kv_num, SEQ_PARTITION_KV_BLK_NUM, num_kv_heads, QUERY_GRP_SZ, KV_BLK_SZ)
 
     print(f"kv_len={kv_len}")
     print(f"head_dim_qk={head_dim_qk}")
-    # print(f"q.shape={q.shape}, q.dtype={q.dtype}")
-    # print(f"k.shape={k.shape}, k.dtype={k.dtype}")
-    print(f"q_broadcasted.shape={q_broadcasted.shape}, q_broadcasted.dtype={q_broadcasted.dtype}")
-    print(f"k_transposed.shape={k_transposed.shape}, k_transposed.dtype={k_transposed.dtype}")
+    print(f"q.shape={q.shape}, q.dtype={q.dtype}")
+    print(f"k.shape={k.shape}, k.dtype={k.dtype}")
     print(f"qk_gn.shape={qk_gn.shape}, qk_gn.dtype={qk_gn.dtype}")
     print(f"qk_ref.shape={qk_gn.shape}, qk_ref.dtype={qk_gn.dtype}")
-    print(f"q_broadcasted.stride()={q_broadcasted.stride()}")
-    print(f"k_transposed.stride()={k_transposed.stride()}")
-    # print(f"q.stride()={q.stride()}")
-    # print(f"k.stride()={k.stride()}")
+    print(f"q.stride()={q.stride()}")
+    print(f"k.stride()={k.stride()}")
     print(f"qk_gn.stride()={qk_gn.stride()}")
     print(f"qk_ref.stride()={qk_ref.stride()}")
-    torch.testing.assert_close(qk_gn, qk_ref, rtol=1e-3, atol=1e-3)
 
+    FLOPS = 2 * batch_size * num_kv_heads * QUERY_GRP_SZ * q_len * kv_len * head_dim_qk / (1e6 * avg_t_us_gn)
+    rw_bytes = batch_size * num_kv_heads * (QUERY_GRP_SZ * q_len * head_dim_qk + kv_len * head_dim_qk) * (torch.finfo(dtype).bits // 8)
+    TFLOPS_gn = FLOPS / (1e6 * avg_t_us_gn)
+    band_width_gn = rw_bytes / (1.024 ** 4 * 1e3 * avg_t_us_gn)
 
-    # TFLOPS = 2 * q_len * kv_len * head_dim_qk / (1e6 * avg_t_us_gn)
-    # band_width = (q_len * head_dim_qk + kv_len * head_dim_qk) * (torch.finfo(dtype).bits // 8) / (1.024 ** 4 * 1e3 * avg_t_us_gn)
+    qk_gn_md5 = hashlib.md5(qk_gn.view(torch.uint8).detach().cpu().numpy().tobytes()).hexdigest()
+    qk_ref_md5 = hashlib.md5(qk_ref.view(torch.uint8).detach().cpu().numpy().tobytes()).hexdigest()
 
-    # qk_gn_md5 = hashlib.md5(qk_gn.view(torch.uint8).detach().cpu().numpy().tobytes()).hexdigest()
-    # qk_ref_md5 = hashlib.md5(qk_ref.view(torch.uint8).detach().cpu().numpy().tobytes()).hexdigest()
+    compare_arrays(qk_gn.to(torch.float32).detach().cpu().numpy(), qk_ref.to(torch.float32).detach().cpu().numpy())
 
-    # qk_gn = qk_gn.to(torch.float32)
-    # compare_arrays(qk_gn.detach().cpu().numpy(), qk_ref.detach().cpu().numpy())
+    print(f"qk_gn_md5={qk_gn_md5}")
+    print(f"qk_ref_md5={qk_ref_md5}")
+    print(f"avg_t_us_gn={avg_t_us_gn:.3f} us, TFLOPS_gn={TFLOPS_gn:.1f} TFLOPS, band_width_gn={band_width_gn:.1f} GB/s")
 
-    # print(f"qk_gn_md5={qk_gn_md5}")
-    # print(f"qk_ref_md5={qk_ref_md5}")
-    # print(f"avg_t_us_gn={avg_t_us_gn:.3f} us, TFLOPS={TFLOPS:.1f} TFLOPS, band_width={band_width:.1f} GB/s")
-
+    qk_ref = qk_ref.to(dtype)
     # torch.testing.assert_close(qk_gn, qk_ref, rtol=1e-3, atol=1e-3)
-    # # torch.testing.assert_close(qk_gn, qk_ref, rtol=1e-3, atol=1e-5)
-    # # torch.testing.assert_close(qk_gn, qk_ref)
+    # torch.testing.assert_close(qk_gn, qk_ref, rtol=1e-2, atol=1e-3)
+    # torch.testing.assert_close(qk_gn, qk_ref, rtol=1.6e-2, atol=1e-5)
+    # torch.testing.assert_close(qk_tn, qk_ref, rtol=1.6e-2, atol=1e-5)
+    torch.testing.assert_close(qk_gn, qk_ref)
     print("\033[92mPASSED\033[0m")
 
 
